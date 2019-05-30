fef5c5edb728e6b5f810a057f87468792f4f2e27
marian <rahman.marian@gmail.com>
2019-05-30 03:35:07 -0400

    updated folders for transfer.

---
+++ 20192805/src/main/java/ApostCardForMe.java
@@ -0,0 +1,13 @@
+public class ApostCardForMe {
+    public static void main(String[] args) {
+        System.out.println("John Smith");
+        System.out.println("123 Main Street");
+        System.out.println("Minneapolis, MN 55555");
+
+                    System.out.println("Jane Williams");
+                    System.out.println("Jane Williams");
+                    System.out.println("354 Mockingbird Ln.");
+                    System.out.println("Portland, OR 99999");
+
+    }
+}
